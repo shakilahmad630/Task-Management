1956a9d42ab0df8e0de17d5bf4244a14e2c387df
Shakil Ahmad <67405423+shakilahmad630@users.noreply.github.com>
2026-06-26 10:22:19 +0530

    setup GitHub Pages deployment with static export

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/Task-Management',
+  images: {
+    unoptimized: true,
+  },
   reactStrictMode: true,
 }
 
